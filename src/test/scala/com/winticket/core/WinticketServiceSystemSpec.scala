c0d985cb0e47af26289a54e4931846084c4373ef
pbernet <paul.bernet@gmail.com>
2016-06-15 12:06:58 +0200

    Change baseURL

--- src/test/scala/com/winticket/core/WinticketServiceSystemSpec.scala
+++ src/test/scala/com/winticket/core/WinticketServiceSystemSpec.scala
@@ -19,66 +19,60 @@ import io.gatling.http.funspec.GatlingHttpFunSpec
 class WinticketServiceSystemSpec extends GatlingHttpFunSpec with Config {
 
   //val baseURL = "http://localhost:9000"
-  val baseURL = "http://winticket.elasticbeanstalk.com"
+  val baseURL = "http://winticket.eu-west-1.elasticbeanstalk.com"
   override def httpConf = {
     super.httpConf.header("Custom-Header", "gatling.io")
     super.httpConf.userAgentHeader("gatling.io")
+    //For testing on localhost:
     //Bind the sockets from a specific local address instead of 127.0.0.1
-    //For now: fill in manually IP of en0/en1
+    //Take IP from en0/en1
     super.httpConf.localAddress("192.168.1.54")
   }
 
-  //simulate a redundant subscription
+  //simulate a redundant subscription -> only one per EMail is accepted for drawing
   spec {
     http("gruenfels/2016/49")
       .get("/gruenfels/2016/49/" + mailAccount1)
       .check(status.is(200))
-    //.check(xpath("/html/body/status/text()").is("OK"))
   }
 
   spec {
     http("gruenfels/2016/49")
       .get("/gruenfels/2016/49/" + mailAccount1)
       .check(status.is(200))
-    //.check(xpath("/html/body/status/text()").is("OK"))
   }
 
-  //subscribe with different accounts for the same event
+  //subscribe with different accounts for the same event -> That is possible
   spec {
     http("gruenfels/2016/49")
       .get("/gruenfels/2016/49/" + mailAccount2)
       .check(status.is(200))
-    //.check(xpath("/html/body/status/text()").is("OK"))
   }
 
   spec {
     http("gruenfels/2016/49")
       .get("/gruenfels/2016/49/" + mailAccount3)
       .check(status.is(200))
-    //.check(xpath("/html/body/status/text()").is("OK"))
   }
 
-  //subscribe with default account for other events
+  //subscribe with default account for other events -> To get more subscription data
   spec {
     http("gruenfels/2016/50")
       .get("/gruenfels/2016/50/" + mailAccount1)
       .check(status.is(200))
-    //.check(xpath("/html/body/status/text()").is("OK"))
   }
 
   spec {
     http("gruenfels/2016/51")
       .get("/gruenfels/2016/51/" + mailAccount1)
       .check(status.is(200))
-    //.check(xpath("/html/body/status/text()").is("OK"))
   }
 
-  //subscribe with default account for 2nd tennant
+  //subscribe with default account for 2nd tennant -> To show the tennant functionality
   spec {
     http("mandant2/2016/1")
       .get("/mandant2/2016/1/" + mailAccount1)
       .check(status.is(200))
-    //.check(xpath("/html/body/status/text()").is("OK"))
   }
 
 }
