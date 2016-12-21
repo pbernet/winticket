f3f82aa153a48378b0aaf7d1d30e0203eb4e96a6
pbernet <paul.bernet@gmail.com>
2016-12-21 10:02:28 +0100

    Add default values and param tennantID

--- src/main/scala/com/winticket/server/DrawingProtocol.scala
+++ src/main/scala/com/winticket/server/DrawingProtocol.scala
@@ -7,8 +7,8 @@ object DrawingProtocol {
   sealed trait DrawingEvent
   case class DrawingActorCreated(actorPath: String) extends DrawingEvent
   case class DrawingCreated(tennantID: String, tennantYear: Int, tennantEMail: String, drawingEventID: String, drawingEventName: String, drawingEventDate: DateTime, drawingLinkToTicket: String, drawingSecurityCodeForTicket: String) extends DrawingEvent
-  case class Subscribed(year: String, eventID: String, email: String, ip: String, date: DateTime) extends DrawingEvent
-  case class SubscriptionRemoved(tennantID: String, tennantYear: Int, drawingEventID: Int, clientIP: Option[String]) extends DrawingEvent
+  case class Subscribed(tennantID: String, year: String, eventID: String, email: String, ip: String, date: DateTime) extends DrawingEvent
+  case class SubscriptionRemoved(tennantID: String = "", tennantYear: Int = 0, drawingEventID: Int = 0, clientIP: Option[String]) extends DrawingEvent
   case class DrawWinnerExecuted(winnerEMail: String) extends DrawingEvent
 
   sealed trait IPCheckEvent
