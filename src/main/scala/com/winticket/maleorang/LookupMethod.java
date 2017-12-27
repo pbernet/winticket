0ad9f25514862b1606d85e0e88e466ae08d77584
pbernet <paul.bernet@gmail.com>
2017-12-27 18:49:24 +0100

    Add 1st working version

---
+++ src/main/scala/com/winticket/maleorang/LookupMethod.java
@@ -0,0 +1,61 @@
+package com.winticket.maleorang;
+
+import com.ecwid.maleorang.MailchimpClient;
+import com.ecwid.maleorang.MailchimpMethod;
+import com.ecwid.maleorang.MailchimpObject;
+import com.ecwid.maleorang.annotation.*;
+import org.apache.commons.codec.digest.DigestUtils;
+
+
+/**
+ * Implement MailChimp lookup method
+ */
+public class LookupMethod {
+    private final String apiKey, listId;
+
+    @Method(httpMethod = HttpMethod.GET, version = APIVersion.v3_0, path = "/lists/{list_id}/members/{subscriber_hash}")
+    private static class LookupRequest extends MailchimpMethod<LookupResponse> {
+        /**
+         * This param will be included into the endpoint path.
+         */
+        @PathParam
+        public final String list_id;
+
+        /**
+         * This param will be included into the endpoint path.
+         */
+        @PathParam
+        public final String subscriber_hash;
+
+
+        public LookupRequest(String listId, String email) {
+            this.list_id = listId;
+            this.subscriber_hash = DigestUtils.md5Hex(email.toLowerCase());
+        }
+    }
+
+    private static class LookupResponse extends MailchimpObject {
+        @Field
+        public String id;
+
+        @Field
+        public String status;
+    }
+
+    public LookupMethod(String apiKey, String listId) {
+        this.apiKey = apiKey;
+        this.listId = listId;
+    }
+
+
+    public String run(String email) throws Exception {
+        MailchimpClient client = new MailchimpClient(apiKey);
+        try {
+            LookupRequest request = new LookupRequest(listId, email);
+            LookupResponse response = client.execute(request);
+            return response.status;
+        } finally {
+            client.close();
+        }
+    }
+}
--- src/main/scala/com/winticket/maleorang/SubscribeUsers.scala
+++ src/main/scala/com/winticket/maleorang/SubscribeUsers.scala
@@ -3,6 +3,7 @@ package com.winticket.maleorang
 import java.io.IOException
 import java.nio.file.Paths
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{FileIO, Framing}
 import akka.stream.{ActorMaterializer, ThrottleMode}
@@ -10,12 +11,18 @@ import akka.util.ByteString
 import com.ecwid.maleorang.method.v3_0.lists.members.EditMemberMethod
 import com.ecwid.maleorang.{MailchimpClient, MailchimpException, MailchimpObject}
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 /**
-  * TODO Verify exceptions before using on prod data
-  * - A user which was set to "unsubscribed" via GUI produces a 404 - this should not happen
-  * - Beautify workflow
+  * Bulk import users to MailChimp via the REST API
+  * The csv file is in format:
+  * valid.email@xxxx.com,firstname,lastname
+  * valid.email2@xxxx.com,firstname2,lastname2
+  * //last line do not remove
+  *
+  * This is an alternative to the bulk import via Admin GUI:
+  * https://kb.mailchimp.com/lists/growth/import-subscribers-to-a-list
   *
   */
 object SubscribeUsers {
@@ -26,7 +33,7 @@ object SubscribeUsers {
   val apiKey = "***"
   val listId = "***"
 
-  val pathToCSV = "/Users/pbernetold/Desktop/gf_newsletter_canditates_test.csv"
+  val pathToCSV = "/path/to/file.csv"
   val client = new MailchimpClient(apiKey)
 
   val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
@@ -39,46 +46,46 @@ object SubscribeUsers {
   }
 
   def main(args: Array[String]): Unit = {
-    FileIO.fromPath(Paths.get(pathToCSV))
+    val closed: Future[Done] = FileIO.fromPath(Paths.get(pathToCSV))
       .via(Framing.delimiter(ByteString("\n"), 1024))
       .map(_.utf8String.split(",").toVector)
-      .throttle(1, 1.second, 1, ThrottleMode.shaping)
+      .throttle(2, 1.second, 5, ThrottleMode.shaping)
       .runForeach(subscribeTo(listId))
+    closed.onComplete { each =>
+      try {
+        client.close()
+        system.terminate()
+      }
+      catch {
+        case e: IOException => e.printStackTrace()
+      }
+    }
   }
 
   private def subscribeTo(listId: String) = {
-    eachRecord: Seq[String] =>
-      println(s"About to subscribe: $eachRecord with length: ${eachRecord.length}")
-      if (isEMailValid(eachRecord.head) && eachRecord.length == 3) {
-          try {
-            val status = new LookupMethod(apiKey, listId).run()
-            System.out.println("STATUS: " + status)
-            if (status == "unsubscribed" || status == "pending" || status == "cleaned") {
-              println(s"User: $eachRecord is in status $status and thus will not be subscribed")
-
-            } else {
-              createOrUpdate(listId, eachRecord)
-            }
-          } catch {
-            case e: MailchimpException => {   //Can I match for the code?
-              System.out.println("MailchimpException: " + e.code)
-              if (e.code == 404) {
-                createOrUpdate(listId, eachRecord)
-              } else {
-                //do nothing
-              }
-            }
-          case e: RuntimeException =>
-            e.printStackTrace()
-        } finally try {
-          client.close()
-          system.terminate()
+    eachRecord: Seq[String] => {
+      val email = eachRecord.head
+      if (isEMailValid(email) && eachRecord.length == 3) {
+        println(s"About to subscribe: $email with correct length: ${eachRecord.length}")
+        try {
+          val status = new LookupMethod(apiKey, listId).run(eachRecord.head)
+          if (status == "unsubscribed" || status == "pending" || status == "cleaned") {
+            println(s"User: $eachRecord is in status: $status and thus will not be subscribed")
+          } else {
+            println(s"User: $eachRecord is on list. Update")
+            createOrUpdate(listId, eachRecord)
+          }
+        } catch {
+          case e: MailchimpException if e.code == 404 => {
+            println(s"User: $eachRecord is not on list. Subscribe")
+            createOrUpdate(listId, eachRecord)
+          }
+          case e: RuntimeException => e.printStackTrace()
         }
-        catch {
-          case e: IOException =>
-            e.printStackTrace()
-        }
+      } else {
+        println(s"$eachRecord does not contain a valid EMail Address OR metadata is not provided, proceed")
       }
+    }
   }
 
   private def createOrUpdate(listId: String, eachRecord: Seq[String]) = {
@@ -88,6 +95,6 @@ object SubscribeUsers {
     createOrUpdate.merge_fields.mapping.put("FNAME", eachRecord(1))
     createOrUpdate.merge_fields.mapping.put("LNAME", eachRecord(2))
     val member = client.execute(createOrUpdate)
-    System.out.println(s"The user: $eachRecord has been successfully subscribed: $member")
+    System.out.println(s"The user: $eachRecord has been successfully created/updated: $member")
   }
 }
